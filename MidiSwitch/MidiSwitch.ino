652170c53a2a5a6007648826c9c756a84c7ad4d6
JD-man <ekgkzmtm@gmail.com>
2024-03-20 14:09:06 +0900

    버튼 중복 클릭 방지

--- MidiSwitch/MidiSwitch.ino
+++ MidiSwitch/MidiSwitch.ino
@@ -1,5 +1,7 @@
 #define BUTTON 2
 
+bool isMidiActivating = false;
+
 void setup() {
   pinMode(BUTTON, INPUT);
 }
@@ -9,9 +11,13 @@ void loop() {
 }
 
 void buttonClicked() {
-  const bool isButtonLow = digitalRead(BUTTON) == LOW;
-  if (isButtonLow) { return; }
-  else {
-    Serial.println("버튼 클릭");
+  const int buttonRead = digitalRead(BUTTON);
+  if (buttonRead == LOW) { 
+    isMidiActivating = false;
+    return;
+  } else {
+    if (isMidiActivating) { return; }
+    Serial.println("버튼 클릭"); // 미디 활성화 함수 넣기
+    isMidiActivating = true;
   }
 }
